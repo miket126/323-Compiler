c4d10f8d8b6bd87befb5335d89348359dbf8233e
Mike Thai <69729071+miket126@users.noreply.github.com>
2023-02-19 21:49:00 -0800

    not working yet

--- lexer.cpp
+++ lexer.cpp
@@ -1,63 +1,187 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <set>
 
-void lexer(std::string& token, char lexeme[BUFSIZ],std::ifstream& file) {
+void lexer(std::string& token, char* lexeme,std::ifstream& file, int& state) {
+    
+    int i = 0;
+    int dfsm[10][5];
+    char c;
+    bool done = false;
+    std::set <char> opsep = {'=','!','>','<','+','-','*','#','(',')','{','}',';',',',' '};
+    std::set <const char*> keyword = {"int","bool","real","if","fi","else","return","put","get","while","endwhile","true","false"};
 
-    int dfsm[9][4];
+    dfsm[1][1] = 3;
+    dfsm[1][2] = 2;
+    dfsm[1][3] = 9;
+    dfsm[1][4] = 9;
 
-    dfsm[0][0] = 3;
-    dfsm[0][1] = 2;
-    dfsm[0][2] = 9;
-    dfsm[0][3] = 9;
-
-    dfsm[1][0] = 9;
-    dfsm[1][1] = 2;
-    dfsm[1][2] = 9;
-    dfsm[1][3] = 4;
-
-    dfsm[2][0] = 5;
-    dfsm[2][1] = 6;
-    dfsm[2][2] = 7;
+    dfsm[2][1] = 9;
+    dfsm[2][2] = 2;
     dfsm[2][3] = 9;
+    dfsm[2][4] = 4;
 
-    dfsm[3][0] = 9;
-    dfsm[3][1] = 8;
-    dfsm[3][2] = 9;
-    dfsm[3][3] = 9;
+    dfsm[3][1] = 5;
+    dfsm[3][2] = 6;
+    dfsm[3][3] = 7;
+    dfsm[3][4] = 9;
 
-    dfsm[4][0] = 5;
-    dfsm[4][1] = 6;
-    dfsm[4][2] = 7;
+    dfsm[4][1] = 9;
+    dfsm[4][2] = 8;
     dfsm[4][3] = 9;
+    dfsm[4][4] = 9;
 
-    dfsm[5][0] = 5;
-    dfsm[5][1] = 6;
-    dfsm[5][2] = 7;
-    dfsm[5][3] = 9;
+    dfsm[5][1] = 5;
+    dfsm[5][2] = 6;
+    dfsm[5][3] = 7;
+    dfsm[5][4] = 9;
 
-    dfsm[6][0] = 5;
-    dfsm[6][1] = 6;
-    dfsm[6][2] = 7;
-    dfsm[6][3] = 9;
+    dfsm[6][1] = 5;
+    dfsm[6][2] = 6;
+    dfsm[6][3] = 7;
+    dfsm[6][4] = 9;
 
-    dfsm[7][0] = 9;
-    dfsm[7][1] = 8;
-    dfsm[7][2] = 9;
-    dfsm[7][3] = 9;
-    
-    dfsm[8][0] = 9;
+    dfsm[7][1] = 5;
+    dfsm[7][2] = 6;
+    dfsm[7][3] = 7;
+    dfsm[7][4] = 9;
+
     dfsm[8][1] = 9;
-    dfsm[8][2] = 9;
+    dfsm[8][2] = 8;
     dfsm[8][3] = 9;
-
-
-
-
-    file.getline(lexeme, BUFSIZ, ' ');
-
+    dfsm[8][4] = 9;
     
+    dfsm[9][1] = 9;
+    dfsm[9][2] = 9;
+    dfsm[9][3] = 9;
+    dfsm[9][4] = 9;
 
+    while (done != true) {
+        file.get(c);
+        if (c == ' ') token = "Space"; done = true; break;
+        
+
+
+        switch (tolower(c))
+        {
+        case '=':
+            lexeme[i++] = c;
+            file.get(c);
+            if (c == ('=' || '>')) {lexeme[i++] = c; token = "Operator"; done = true;}
+            else { file.unget(); }
+            break;
+
+        case '!':
+            lexeme[i++] = c;
+            file.get(c);
+            if (c == '=') {lexeme[i++] = c; token = "Operator"; done = true;}
+            else { token = "Unknown"; file.unget();}
+            break;
+
+        case '<':
+            lexeme[i++] = c;
+            done = true;
+            file.get(c);
+            if (c == ' ') { token = "Operator"; state == 9;}
+            else if (c == '=') {lexeme[i++] = c; token = "Operator"; done = true;}
+            else { file.unget(); }
+            break;
+
+        case '>':
+        case '+':
+        case '-':
+        case '*':
+            lexeme[i++] = c;
+            token = "Operator";
+            done = true;
+            break;
+        
+        case '#':
+        case '|':
+        case '(':
+        case ')':
+        case '{':
+        case '}':
+        case ';':
+        case ',':
+            lexeme[i++] = c; 
+            token = "Separator"; 
+            done = true; 
+            break;
+
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            state = dfsm[state][1]; 
+            if (state = 3 || 5 || 6 || 7) token = "Identifier";
+            lexeme[i++] = c;
+
+            file.get(c);
+            if (opsep.find(c) != opsep.end()) { done = true; file.unget(); break; }
+            else if (c == '_') { lexeme[i++] = c; state = dfsm[state][3]; }
+            else if (c == '.') { lexeme[i++] = c; state = dfsm[state][4]; }
+            else file.unget();
+                
+            break; //accepting is 3,5,6,7
+
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            state = dfsm[state][2]; 
+            if (state = 2) token = "Integer";
+            else if (state = 8) token = "Real";
+            else if (state = 6) token = "Identifier";
+            lexeme[i++] = c;
+
+            file.get(c);
+            if (opsep.find(c) != opsep.end()) { done = true; file.unget(); break; }
+            else if (c == '.') { lexeme[i++] = c; state = dfsm[state][4]; }
+            else file.unget();
+
+            break; //accepting is 2,8
+        
+        default:
+            token == "Unknown";
+            done = true;
+            break;
+        }
+    }
+    
+    if (keyword.find(lexeme) != keyword.end()) token = "Keyword";
+    if (state == 9) token = "Unknown";
+            std::cout << "here1\n";
+    lexeme[i] = 'c';
 
 }
 
@@ -72,20 +196,26 @@ int main(int argc, const char * argv[])
     std::string filename = argv[1];
     std::ifstream file(filename);
     std::string token;
-    char lexeme[BUFSIZ];
+    char *lexeme = new char[BUFSIZ];
+    int state = 1;
 
     if (!file.is_open()) {
       std::cerr << "\n\nWarning, could not open file '" << filename << "'\n\n";
       exit(-1);
     }
 
-    std::cout << "token\t\t\tlexeme\n"; 
+    std::cout << "Token\t\t\tLexeme\n"; 
 
     while (!file.eof()) {
-        lexer(token,lexeme,file);
-        std::cout << token << "\t\t\t" << lexeme << std::endl;
+        lexer(token,lexeme,file,state);
+        std::cout << "here\n";
+        if (token != "Space") {
+            std::cout << token << "\t\t\t" << lexeme << std::endl;
+        }
+        lexeme[BUFSIZ] = '\0';
+        state = 1;
     }
 
-
+    file.close();
     return 0;
 }
